626ef45381f333c41558ef840c680d97b2ffb9f7
bento <88791367+bento170502@users.noreply.github.com>
2022-08-06 13:32:31 +0700

    Update PerfectNumberTest.java

--- src/test/java/com/thealgorithms/maths/PerfectNumberTest.java
+++ src/test/java/com/thealgorithms/maths/PerfectNumberTest.java
@@ -1,20 +1,3 @@
-// package com.thealgorithms.maths;
-// import org.junit.jupiter.api.Test;
-// import static org.junit.jupiter.api.Assertions.assertEquals;
-
-// class PerfectNumberTest{
-//     @Test
-//     //valid partition
-//     public void TestPerfectNumber() {
-//         assertEquals(true, PerfectNumber.isPerfectNumber(28));
-//     }
-
-//     @Test
-//     //invalid partition
-//     public void TestNotPerfectNumber() {
-//         assertEquals(false, PerfectNumber.isPerfectNumber(29));
-//     }
-// }
 
 package com.thealgorithms.maths;
 
