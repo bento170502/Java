dc680a1806b9fd8e69c34684bbb629649fcd5899
bento <88791367+bento170502@users.noreply.github.com>
2022-08-06 13:30:53 +0700

    Update PerfectNumberTest.java

--- src/test/java/com/thealgorithms/maths/PerfectNumberTest.java
+++ src/test/java/com/thealgorithms/maths/PerfectNumberTest.java
@@ -1,17 +1,42 @@
-package com.thealgorithms.maths;
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+// package com.thealgorithms.maths;
+// import org.junit.jupiter.api.Test;
+// import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class PerfectNumberTest{
+// class PerfectNumberTest{
+//     @Test
+//     //valid partition
+//     public void TestPerfectNumber() {
+//         assertEquals(true, PerfectNumber.isPerfectNumber(28));
+//     }
+
+//     @Test
+//     //invalid partition
+//     public void TestNotPerfectNumber() {
+//         assertEquals(false, PerfectNumber.isPerfectNumber(29));
+//     }
+// }
+
+package com.thealgorithms.maths;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class PerfectNumberTest {
     @Test
-    //valid partition
-    public void TestPerfectNumber() {
-        assertEquals(true, PerfectNumber.isPerfectNumber(28));
+    void test1() {
+        Assertions.assertTrue(PerfectNumber.isPerfectNumber(28));
+    }
+    @Test
+    void test2() {
+        Assertions.assertTrue(PerfectNumber.isPerfectNumber(6));
+    }
+    @Test
+    void test3() {
+        Assertions.assertFalse(PerfectNumber.isPerfectNumber(27));
     }
 
     @Test
-    //invalid partition
-    public void TestNotPerfectNumber() {
-        assertEquals(false, PerfectNumber.isPerfectNumber(29));
+    void test4() {
+        Assertions.assertFalse(PerfectNumber.isPerfectNumber(29));
     }
 }
